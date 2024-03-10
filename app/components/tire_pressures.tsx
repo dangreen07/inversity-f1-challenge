d7676fb184b520037525a55bd5a3b6d6fa699708
Daniel Green <dangreen07@outlook.com>
2024-03-10 18:54:26 +0000

    Setup tire page with database

--- app/components/tire_pressures.tsx
+++ app/components/tire_pressures.tsx
@@ -1,25 +1,27 @@
-export default function TirePressures() {
+import { TireData } from "../tire/page";
+
+export default async function TirePressures({latestData}: {latestData: TireData}) {
     return (
         <div className="bg-white rounded-2xl w-2/5 flex flex-col" role="tire_pressures">
             <p className="font-kanit not-italic font-medium text-5xl flex items-center justify-center">Tire Pressures</p>
             <div className="flex flex-row gap-16 justify-center items-center py-6">
                 <div className="flex flex-col w-2/5 bg-[#0F0326] rounded-2xl">
                     <p className={`font-kanit non-italic font-normal text-xl items-center pl-2 pt-3 text-white`}>Front Left:</p>
-                    <p className={`text-4xl font-kanit non-italic font-semibold items-center text-center p-5 text-white`}>27.0 psi</p>
+                    <p className={`text-4xl font-kanit non-italic font-semibold items-center text-center p-5 text-white`}>{latestData?.frontLeftTirePressure} psi</p>
                 </div>
                 <div className="flex flex-col w-2/5 bg-[#8EE3EF] rounded-2xl">
                     <p className={`font-kanit non-italic font-normal text-xl items-center pl-2 pt-3 text-black`}>Front Right:</p>
-                    <p className={`text-4xl font-kanit non-italic font-semibold items-center text-center p-5 text-black`}>24.3 psi</p>
+                    <p className={`text-4xl font-kanit non-italic font-semibold items-center text-center p-5 text-black`}>{latestData?.frontRightTirePressure} psi</p>
                 </div>
             </div>
             <div className="flex flex-row gap-16 justify-center items-center py-6">
                 <div className="flex flex-col w-2/5 bg-[#8EE3EF] rounded-2xl">
                     <p className={`font-kanit non-italic font-normal text-xl items-center pl-2 pt-3 text-black`}>Back Left:</p>
-                    <p className={`text-4xl font-kanit non-italic font-semibold items-center text-center p-5 text-black`}>24.3 psi</p>
+                    <p className={`text-4xl font-kanit non-italic font-semibold items-center text-center p-5 text-black`}>{latestData?.backLeftTirePressure} psi</p>
                 </div>
                 <div className="flex flex-col w-2/5 bg-[#0F0326] rounded-2xl">
                     <p className={`font-kanit non-italic font-normal text-xl items-center pl-2 pt-3 text-white`}>Back Right:</p>
-                    <p className={`text-4xl font-kanit non-italic font-semibold items-center text-center p-5 text-white`}>24.5 psi</p>
+                    <p className={`text-4xl font-kanit non-italic font-semibold items-center text-center p-5 text-white`}>{latestData?.backRightTirePressure} psi</p>
                 </div>
             </div>
         </div>
--- app/tire/page.tsx
+++ app/tire/page.tsx
@@ -3,24 +3,63 @@ import Heading from "../ui/heading";
 import DataBox from "../components/databox";
 import TirePressures from "../components/tire_pressures";
 import TirePerformanceGraph from "../components/tire_performance";
+import prisma from "@/lib/prisma";
 
-export default function TyrePage() {
+// Null error output object
+const errorOutput = {
+    id:"Error",
+    surfaceTemp: 27.6,
+    tireCompound: "C1",
+    pitStopIn: -1,
+    frontLeftTirePressure: 2,
+    frontRightTirePressure: 2,
+    backLeftTirePressure: 2,
+    backRightTirePressure: 2,
+    date: new Date("2004-10-19 10:23:54")
+};
+
+export async function getLatestTireData() {
+    try {
+        const feed = prisma.tire.findFirst({
+            orderBy: {date: 'desc'}
+        });
+        return feed;
+    } catch (error) {
+        return errorOutput;
+    }
+    
+}
+
+export type TireData = {
+    id: string;
+    surfaceTemp: number;
+    tireCompound: string;
+    pitStopIn: number;
+    frontLeftTirePressure: number;
+    frontRightTirePressure: number;
+    backLeftTirePressure: number;
+    backRightTirePressure: number;
+    date: Date;
+}
+
+export default async function TirePage() {
+    const latestData = await getLatestTireData();
     return (
         <main className="flex flex-col w-full h-full">
             <Heading pageName="Tire" />
             <div className="flex flex-row justify-center items-center gap-20 pt-7">
                 <div className="rounded-2xl bg-[#5386E4] w-1/4">
-                    <DataBox dataName="Current Surface Temperature" data="98" symbol="°C" textBlack={true}/>
+                    <DataBox dataName="Current Surface Temperature" data={latestData?.surfaceTemp.toString() ?? "87"} symbol="°C" textBlack={true}/>
                 </div>
                 <div className="rounded-2xl bg-[#0F0326] w-1/4">
-                    <DataBox dataName="Current Tire Compound" data="C3" symbol="" textBlack={false} />
+                    <DataBox dataName="Current Tire Compound" data={latestData?.tireCompound ?? "C1"} symbol="" textBlack={false} />
                 </div>
                 <div className="rounded-2xl bg-[#8EE3EF] w-1/4">
-                    <DataBox dataName="Pit Stop In" data="8" symbol=" Laps" textBlack={true}/>
+                    <DataBox dataName="Pit Stop In" data={latestData?.pitStopIn.toString() ?? "8"} symbol=" Laps" textBlack={true}/>
                 </div>
             </div>
             <div className="flex flex-row w-full h-full gap-16 justify-center mt-5">
-                <TirePressures />
+                <TirePressures latestData={latestData ?? errorOutput}/>
                 <TirePerformanceGraph />
             </div>
         </main>
